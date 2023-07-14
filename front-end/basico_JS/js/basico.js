f9d33c4d100fc6ea0f3bc50dba67f2294bf594f1
Jiulio Gusmao <jiuliogusmao@gmail.com>
2023-07-13 21:45:34 -0300

    basico do JavaScript

---
+++ front-end/basico_JS/js/basico.js
@@ -0,0 +1,17 @@
+console.log("Hello world, no arquivo js!")
+console.log(1 + "1")
+console.log(1 - "1")
+
+var x = 10
+let y = 11
+
+var x = 10
+//let y = 11
+
+console.log(typeof(x))
+
+console.log(aula * 2)
+
+let numero = Number(50)
+
+console.log(typeof(numero))
