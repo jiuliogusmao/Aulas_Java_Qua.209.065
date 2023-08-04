310aec069b6039696740fae8ea50f339bd5b8b32
Jiulio Gusmao <jiuliogusmao@gmail.com>
2023-08-03 21:22:51 -0300

    alterando o generation type para identity

--- projetospring/src/main/java/br/com/senai/model/Produto.java
+++ projetospring/src/main/java/br/com/senai/model/Produto.java
@@ -1,6 +1,11 @@
 package br.com.senai.model;
 
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,13 +15,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Produto {
+public class Produto implements Serializable{
 	
+	
+	private static final long serialVersionUID = 1L;
+
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 100, nullable = false)
 	private String nome;
+	
+	@Column(length = 100, nullable = false)
 	private String categoria;
+	
+	@Column(length = 100, nullable = false)
 	private int quantidade;
+	
+	@Column(length = 100, nullable = false)
 	private double preco;
 	
 }
